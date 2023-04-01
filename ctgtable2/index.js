4c2a5e6f557db827266d44beca8a5c8bfb8fbe92
mmk666 <129168458+mmk666@users.noreply.github.com>
2023-04-02 02:16:21 +0530

    1 file modified

--- ctgtable2/index.js
+++ ctgtable2/index.js
@@ -10,12 +10,34 @@ const groupByKey = (data, key) => {
   }, {});
 };
 
-export const getTabledata = (
-  data,
-  basekey,
-  filterkey,
-  filterval,
-  timePeriod
-) => {
-  
+export const getTabledata = (data, basekey, filterkey, filterval) => {
+  const filterData = data.filter((item) => item[filterkey] === filterval);
+  const dates = [...new Set(filterData.map((item) => item.FISCAL_YEAR_WEEK))];
+  const Total = filterData.reduce((accum, item) => {
+    const obj = { ...accum };
+    if (accum[item['FISCAL_YEAR_WEEK']]) {
+      obj[item['FISCAL_YEAR_WEEK']] = (
+        Number(accum[item['FISCAL_YEAR_WEEK']]) + Number(item['CW_CTG_PER_VAL'])
+      ).toFixed(2);
+    } else {
+      obj[item['FISCAL_YEAR_WEEK']] = Number(item['CW_CTG_PER_VAL']).toFixed(2);
+    }
+    return obj;
+  }, {});
+  const products = [
+    ...new Set(filterData.map((item) => item[basekey])),
+    'Total',
+  ];
+  const groupByProducts = groupByKey(filterData, basekey);
+  for (const product in groupByProducts) {
+    const obj = groupByKey(groupByProducts[product], 'FISCAL_YEAR_WEEK');
+    const item = dates.reduce((accum, item) => {
+      accum[item] = obj[item]
+        ? Number(obj[item][0]['CW_CTG_PER_VAL']).toFixed(2)
+        : '-';
+      return accum;
+    }, {});
+    groupByProducts[product] = item;
+  }
+  return { ...groupByProducts, Total };
 };
